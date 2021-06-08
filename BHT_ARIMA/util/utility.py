d88c7cedaa9c60b317d501eb595ec6f6ee72dced
SHI-Qiquan <61440546+SHI-Qiquan@users.noreply.github.com>
2021-06-08 09:29:04 +0800

    Update utility.py

--- BHT_ARIMA/util/utility.py
+++ BHT_ARIMA/util/utility.py
@@ -1,5 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+# Copyright (C) 2020. Huawei Technologies Co., Ltd. All rights reserved.
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# MIT License for more details.
+
+
 import pickle as pkl
 import pandas as pd
 import numpy as np
